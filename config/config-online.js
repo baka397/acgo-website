a75f670516f04fa3abe02d5148cb4f1b424e5d48
bakajinsei <cqggff@live.com>
2017-01-23 15:33:19 +0800

    Update package & config

--- config/config-online.js
+++ config/config-online.js
@@ -7,7 +7,8 @@ module.exports = {
     tokenSecure: true,
     clientPath:'https://www.acgo.club/client',
     //api信息
-    apiPath:'https://api.acgo.club/api/v1',
+    //apiPath:'https://api.acgo.club/api/v1',
+    apiPath:'http://127.0.0.1:8000/api/v1',
     apiAlias:'website',
     apiKey:'5885a22ecb8bcd6da6d69d7c'
 }
